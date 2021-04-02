ead6ab01e0ac2757af439882d0595d05b7210249
Jenna Garrison <78498827+jenna4285@users.noreply.github.com>
2021-04-01 20:36:49 -0500

    update file names and cleanup code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # WorkDayScheduler
+
+Utilize the day planner to input to-do's! 
+
+This page stores your to-do items locally & indicates past, present, and future times via color-coding!
+
+https://jenna4285.github.io/WorkDayScheduler/
--- script.js
+++ script.js
@@ -1,60 +1,72 @@
-// variable for time/date at header
 var today = moment();
-// var input = $(".input");
-
-// variables for local storage 
-var timeColumn = $(".timeColumn");
 var saveButton = $(".saveBtn");
+var now = moment().format("HH");
+// var now = 12;
+var desc = $(".description");
+console.log(now);
+
+var timeColumn = $(".time-block");
 console.log(timeColumn);
+console.log(timeColumn[0].id);
+
 
 // Display Day/Date at Header
 $("#currentDay").text(today.format("dddd, MMM Do, YYYY"));
 
-// local storage for "input"
+// render page load persist local storage
+
+    $('#9am').val(localStorage.getItem('9am'));
+    $('#10am').val(localStorage.getItem('10am'));
+    $('#11am').val(localStorage.getItem('11am'));
+    $('#12pm').val(localStorage.getItem('12pm'));
+    $('#13pm').val(localStorage.getItem('1pm'));
+    $('#14pm').val(localStorage.getItem('2pm'));
+    $('#15pm').val(localStorage.getItem('3pm'));
+    $('#16pm').val(localStorage.getItem('4pm'));
+    $('#17pm').val(localStorage.getItem('5pm'));
+
+
 function saveFormInput (event) {
     event.preventDefault();
     var timeBlock = $(this).val();
-    
-    var toDoItem = $('[name=fname]').val();
-    console.log(toDoItem);
+    console.log(timeBlock);
 
-    $(".timeColumn").each(function( index, time ) {
-        var x = time;
+    $(".description").each(function( index, text ) {
+        var x = $(text).attr("id");
         console.log(x);
+        var toDoItem = $(text).val();
+        console.log(toDoItem);
+        if (timeBlock===x) {
+            localStorage.setItem(timeBlock, toDoItem);
+        }
     });
 
-
-
-    if (!toDoItem) {
-        console.log("No to do items in form!");
-        return;
-    } else {
-        localStorage.setItem(timeBlock, toDoItem);
-    }
-
-    
 }
 
+// if statement for color class (past present future)
 
-// function to change color for past/present/future
+    // for (var i=0; i<timeColumn.length; i++){
+     $(".hour").each (function (){   
+       var timeStamp = $(this).attr("value");
 
-// get data from time column for use in for each loop
-// $(".timeColumn").map(function() {
-//     return((timeColumn).outerText());
-// }).get().join();
+    //    console.log(timeStamp);
+    //    console.log(now);
+    //    console.log($(this));
 
-// console.log("after joining"+timeColumn);
+    if (timeStamp===now) {
+       desc.removeClass("past");
+       desc.removeClass("future");
+       desc.addClass("present");
+    } else if (timeStamp<now) {
+       desc.addClass("past");
+       desc.removeClass("future");
+       desc.removeClass("present");
+    } else if (timeStamp>now) {
+       desc.removeClass("past");
+       desc.addClass("future");
+       desc.removeClass("present");
+    }
+ }
+)  
 
-// run for each loop to change class/color
-// var currentHour = moment().format("hh mm");
-// console.log (currentHour);
-
-//  $(".timeColumn").each(function (i) {
-//      if (timeColumn===currentHour) {
-//        input.addClass("present");
-//      } else {
-//          input.addClass("future");
-//      }
-// //  })
-
- saveButton.on('click', saveFormInput);
+saveButton.on('click', saveFormInput);
